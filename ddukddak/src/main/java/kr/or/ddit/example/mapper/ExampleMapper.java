145c92ee0e5155639e4b13166c8a11545f995443
PC-23 <PC-23@DESKTOP-4OSK6AA>
2023-11-29 10:46:53 +0900

    최초커밋

---
+++ ddukddak/src/main/java/kr/or/ddit/example/mapper/ExampleMapper.java
@@ -0,0 +1,5 @@
+package kr.or.ddit.example.mapper;
+
+public interface ExampleMapper {
+
+}
---
+++ ddukddak/src/main/java/kr/or/ddit/example/service/ExampleServiceImpl.java
@@ -0,0 +1,8 @@
+package kr.or.ddit.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleServiceImpl {
+
+}
