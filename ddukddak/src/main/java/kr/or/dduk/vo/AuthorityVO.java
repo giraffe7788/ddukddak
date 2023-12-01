0cabbcef6ad3e730917be2976370e55192f68cc3
PC-02 <PC-02@DESKTOP-CES6SF6>
2023-12-01 10:02:19 +0900

    직원 VO, 권한 VO 주석 추가

--- ddukddak/src/main/java/kr/or/dduk/vo/AuthorityVO.java
+++ ddukddak/src/main/java/kr/or/dduk/vo/AuthorityVO.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class AuthorityVO {
-	private String empNo;
-	private String authName;
+	private String empNo;		//사번
+	private String authName;	//권한명
 }
--- ddukddak/src/main/java/kr/or/dduk/vo/EmployeeVO.java
+++ ddukddak/src/main/java/kr/or/dduk/vo/EmployeeVO.java
@@ -7,21 +7,21 @@ import lombok.Data;
 
 @Data
 public class EmployeeVO {
-	private String empNo;			//사번
-    private String empPh;
-    private String empName;
-    private String empPw;
-    private String empAcntState;
-    private Date empIn;
-    private Date empOut;
-    private String empMail;
-    private String empReg;
-    private int empZip;
-    private String empAdd1;
-    private String empAdd2;
-    private String fileCd;
-    private String empDeptCd;
-    private String empJbpsCd;
+	private String empNo;			//사번   
+    private String empPh;           //전화번호 
+    private String empName;         //성명 
+    private String empPw;           //비밀번호 
+    private String empAcntState;    //비밀번호 오류 횟수 
+    private Date empIn;             //입사일 
+    private Date empOut;            //퇴사일 
+    private String empMail;         //이메일 
+    private String empReg;          //등록일자
+    private int empZip;             //우편번호
+    private String empAdd1;         //주소
+    private String empAdd2;         //상세주소
+    private String fileCd;          //파일코드
+    private String empDeptCd;       //직무코드
+    private String empJbpsCd;       //직책코드
     
-    private List<AuthorityVO> authorityVOList;
+    private List<AuthorityVO> authorityVOList; //권한 VO 리스트
 }
