bfe3aff051cb53991443bca6e8402d55926174d5
PC-02 <PC-02@DESKTOP-CES6SF6>
2023-12-01 10:08:54 +0900

    문자 관련 VO 추가

---
+++ ddukddak/src/main/java/kr/or/dduk/vo/MmsContVO.java
@@ -0,0 +1,14 @@
+package kr.or.dduk.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MmsContVO {
+	private String mmsCd;		//문자발송코드
+	private String mmsFormCd;   //양식코드
+	private String mmsSent;     //발신자
+	private String mmsRecv;     //수신자
+	private Date mmsDate;       //발송일시
+}
---
+++ ddukddak/src/main/java/kr/or/dduk/vo/MmsFormVO.java
@@ -0,0 +1,10 @@
+package kr.or.dduk.vo;
+
+import lombok.Data;
+
+@Data
+public class MmsFormVO {
+	private String mmsFormCd;		//양식코드
+	private String mmsFormCont;     //양식내용
+	private String mmsFormType;     //양식분류
+}
