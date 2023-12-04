631f01525b117a4b942b148c349c7daa0b7bcf55
PC-21 <PC-21@DESKTOP-2VCJAA4>
2023-12-04 20:35:04 +0900

    2023.12.04 chatting

---
+++ ddukddak/src/main/java/kr/or/dduk/vo/ChatVO.java
@@ -0,0 +1,14 @@
+package kr.or.dduk.vo;
+
+import lombok.Data;
+
+@Data
+public class ChatVO {
+	private int chatRmListNo;
+	private int chatNo;		//채팅메시지번호
+	private String chatRmNo;
+	private String chatCont;	//채팅내용
+	private String chatDate;	//채팅날짜
+	private String empNo;
+	private String chatFileCd;		//첨부파일
+}
